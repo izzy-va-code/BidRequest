fea2f36599598fc0fbab6c3639b380ef7fd62829
izzy-va-code <vladislavizuita@gmail.com>
2019-10-21 16:30:17 +0300

    upd2

--- server.js
+++ server.js
@@ -1,70 +1,119 @@
 "use strict"
 const fs = require('fs')
+const parser = require('ua-parser-js')
+const csvtojson = require('csvtojson')
+const json2csv = require('json2csv').parse
+const djv = require('djv')
 const fastify = require('fastify')({
     logger: true
 })
-const validationSchema = {
-    schema: {
-        body: {
-            type: 'object',
-            properties: {
-                id: {type: 'string'},
-                imp: {type: 'array', items: { type: 'object' }},
-                site: {type: 'object'},
-                app: {type: 'object'},
-                device: {type: 'object'},
-                user: {type: 'object'},
-                test: {type: 'integer'},
-                at: {type: 'integer'},
-                cur: {type: 'array',  items: { type: 'string' }},
-                bcat: {type: 'array', items: { type: 'string' }},
-                badv: {type: 'array', items: { type: 'string' }},
-                source: {type: 'object'},
-                regs: {type: 'object'},
-                ext: {type: 'object'},
-            },
-            required: ['id', 'imp', 'at' , 'cur']
-        }
-    }
-}
 
+const env = new djv({ version: 'draft-04' })
+const requestSchema = require('./request-schema')
+const devicesListPath = './DevicesList.csv'
+const fields = ["devicetype", "make", "model", "os", "osv", "hwv"]
+var optsJson2Csv = { fields, header: false }
+
+// Headers check on start
+try {
+    if(fs.existsSync(devicesListPath)){
+        csvtojson().fromFile(devicesListPath).on('header', (headers) => {
+            console.log("File has headers: " + headers)
+            console.log(fields)
+            if(headers.equals(fields)){
+                console.log("Headers equal fields.")
+                optsJson2Csv.header = false
+            }
+            else {
+                console.log("Headers are not equal to fields. Setting headers.")
+                optsJson2Csv.header = true
+            }
+        })
+    }
+    else{
+        optsJson2Csv.header = true
+    }
+} catch(err) {
+    console.error(err)
+}
 // Declare routes
-fastify.post('/bidrequest',{validationSchema, attachValidation:true}, function (request, reply) {
-    if (request.validationError) {
-        reply.code(400).send(request.validationError)
+fastify.post('/bidrequest', function (request, reply) {
+    env.addSchema('requestSchema', requestSchema)
+
+    let validError = env.validate('requestSchema', request.body)
+
+    if(validError){
+        console.log("Validation error:")
+        console.log(validError)
+        reply.code(400).send(validError)
     }
     else {
-        var res = parseDevice(request.body)
+        console.log("Validation successful")
+        let res = parseUserAgent(request.body)
         if(res){
-            fs.appendFile('DevicesList.txt', JSON.stringify(res) + '\n', (err) => {
-            if (err) throw err
-            console.log('The file has been saved!')
-            reply   .code(200)
-                    .header('Content-Type', 'application/json; charset=utf-8')
-                    .send({ Message: 'Succesfull' })
+            const csv = json2csv(res, optsJson2Csv)
+            fs.appendFile(devicesListPath, csv + '\n', (err) => {
+                if (err) throw err
+                console.log('The file has been saved!')
+                reply   .code(200)
+                        .header('Content-Type', 'application/json; charset=utf-8')
+                        .send({ Message: 'Succesfull' })
+                optsJson2Csv.header = false
             })
-        }
-        else console.log("Not");
-    }
 
+        }
+        else console.log("No device object")
+    }
 })
 
-// Parsing function
-function parseDevice(obj){
-    if(obj.device){
-        let dev = obj.device;
-        if(dev.devicetype || dev.make || dev.model || dev.os || dev.osv || dev.hwv){
-            let parseResult= {};
-            parseResult.id = obj.id
-            parseResult.devicetype = dev.devicetype
-            parseResult.make = dev.make
-            parseResult.model = dev.model
-            parseResult.os = dev.os
-            parseResult.osv = dev.osv
-            parseResult.hwv = dev.hwv
-            console.log(JSON.stringify(parseResult))
-            return parseResult
+// Equals function
+Array.prototype.equals = function (array) {
+    if (!array)
+        return false;
+
+    if (this.length != array.length)
+        return false;
+
+    for (var i = 0, l=this.length; i < l; i++) {
+        if (this[i] instanceof Array && array[i] instanceof Array) {
+            if (!this[i].equals(array[i]))
+                return false;
         }
+        else if (this[i] != array[i]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+// Parsing function
+function parseUserAgent(obj){
+    if(obj.device){
+        var result = {  devicetype:undefined,
+                        make:undefined,
+                        model:undefined,
+                        os:undefined,
+                        osv:undefined,
+                        hwv:undefined }
+
+        var ua = parser(obj.device.ua)
+
+        if(ua.device.type) result.devicetype = ua.device.type
+
+        if(ua.device.vendor) result.make = ua.device.vendor
+
+        if(ua.device.model) result.model = ua.device.model
+
+        if(ua.os.name) result.os = ua.os.name
+
+        if(ua.os.version) result.osv = ua.os.version
+
+        if(obj.device.hwv) result.hwv = obj.device.hwv
+
+        console.log(ua)
+        console.log(result)
+        return result
     }
     else return null
 }
